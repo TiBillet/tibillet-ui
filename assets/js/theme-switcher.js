18aee642d34f64fa39d071a751e879ee5faa6b92
Diane A. K. Navarro <pro@kayathommy.fr>
2024-09-10 16:12:20 +0200

    better theme switcher

---
+++ assets/js/theme-switcher.js
@@ -0,0 +1,24 @@
+/** Theme switcher based on local store first, user preferences last */
+
+const query = window.matchMedia('(prefers-color-scheme: dark)')
+const target = document.querySelector('html')
+const toggle = document.querySelector('#themeToggle')
+
+const refresh = ({ matches }) => {
+    let theme = localStorage.getItem('theme') || (matches ? 'dark' : 'light')
+    
+    target.dataset.bsTheme = theme
+    toggle.checked = theme === 'dark'
+}
+
+const update = ({ target: { checked }}) => {
+    let theme = checked ? 'dark' : 'light'
+
+    target.dataset.bsTheme = theme
+
+    localStorage.setItem('theme', theme)
+}
+
+refresh(query)
+query.addEventListener('change', refresh)
+toggle.addEventListener('click', update)
