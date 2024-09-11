a00af9e357daa4c1e686ebb3a1b8fb47a6e7e778
Diane A. K. Navarro <pro@kayathommy.fr>
2024-09-11 12:33:43 +0200

    better filters and filters tags

---
+++ assets/js/filter-tags.js
@@ -0,0 +1,20 @@
+/** Search bar tag filtering based on query string */
+
+const params = new URLSearchParams(window.location.search)
+const tagTypes = ['genre', 'price-cap', 'location', 'accessibility']
+const target = document.querySelector('#filterTags')
+const tags = tagTypes.reduce((acc, type) =>
+    params.has(type) ? [...acc, ...params.get(type).split(',')] : acc,
+    []
+)
+
+const renderTag = label =>
+    `<button class="btn btn-info d-none d-md-block js-filter-tag" type="button" style="border-radius: 0;">
+        ${label} <i class="bi bi-x"></i>
+    </button>`
+
+target.innerHTML = tags.map(renderTag).join('\n')
+
+Array.from(target.querySelectorAll('.js-filter-tag')).forEach(child =>
+    child.addEventListener('click', _ => child.remove())
+)
